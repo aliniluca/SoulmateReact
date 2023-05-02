c1c7885a0f78d8f8ec472746f35e522c708e8d2a
aliniluca <115418094+aliniluca@users.noreply.github.com>
2023-05-02 23:27:15 +0200

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,23 +1,82 @@
-const handleSubmit = async (e) => {
-  e.preventDefault();
+import React, { useState } from 'react';
+import axios from 'axios';
 
-  if (!agree) {
-    setThickboxOpen(true);
-    return;
-  }
+function Modal({ open, onClose }) {
+  if (!open) return null;
 
-  // Send data to your backend
-  try {
-    const response = await axios.post('/subscribe', {
-      email,
-      firstName,
-      birthdate
-    });
+  return (
+    <div className="modal-backdrop" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <p>Please agree to receive emails from us before submitting the form.</p>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+}
 
-    // Handle success (e.g., show a success message to the user)
-  } catch (error) {
-    console.error('Error sending data to the backend:', error);
-  }
-};
+function Form() {
+  const [agree, setAgree] = useState(false);
+  const [thickboxOpen, setThickboxOpen] = useState(false);
+  const [email, setEmail] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [birthdate, setBirthdate] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (agree) {
+      try {
+        await axios.post('/api/send-email', { email, firstName, birthdate });
+        // Handle success
+      } catch (error) {
+        // Handle error
+      }
+    } else {
+      setThickboxOpen(true);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+
+        <label htmlFor="birthdate">Birthdate:</label>
+        <input
+          type="date"
+          id="birthdate"
+          value={birthdate}
+          onChange={(e) => setBirthdate(e.target.value)}
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="agree">
+          <input
+            type="checkbox"
+            id="agree"
+            checked={agree}
+            onChange={(e) => setAgree(e.target.checked)}
+          />
+          I agree to receive emails from Divine Darling's Twinflame Reading.
+        </label>
+
+        <button type="submit">Submit</button>
+      </form>
+      <Modal open={thickboxOpen} onClose={() => setThickboxOpen(false)} />
+    </>
+  );
+}
 
 export default Form;
