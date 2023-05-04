5866bb75c776b6120d10b4f6186363487057d737
aliniluca <115418094+aliniluca@users.noreply.github.com>
2023-05-04 21:49:25 +0200

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -22,45 +22,44 @@ const Form = () => {
           <div className="form">
             <button className="close-modal" onClick={closeModal}>X</button>
             <h2>Just answer a few simple questions so we can draw your sketch accurately...</h2>
-            { <label htmlFor="interestedIn">Who are You Interested In?</label>
-          <select id="interestedIn">
-            <option>I Like Men</option>
-            <option>I Like Women</option>
-          </select>
+            <>
+              <label htmlFor="interestedIn">Who are You Interested In?</label>
+              <select id="interestedIn">
+                <option>I Like Men</option>
+                <option>I Like Women</option>
+              </select>
 
-          <label htmlFor="relationshipStatus">What is Your Relationship Status?</label>
-          <select id="relationshipStatus">
-            <option>Select Your Status</option>
-            {/* Add other options here */}
-          </select>
+              <label htmlFor="relationshipStatus">What is Your Relationship Status?</label>
+              <select id="relationshipStatus">
+                <option>Select Your Status</option>
+                {/* Add more options here */}
+              </select>
 
-          <label htmlFor="zodiacSign">What is Your Zodiac Sign?</label>
-          <select id="zodiacSign">
-            <option>Select Your Sign</option>
-            {/* Add other options here */}
-          </select>
+              <label htmlFor="zodiacSign">What is Your Zodiac Sign?</label>
+              <select id="zodiacSign">
+                <option>Select Your Sign</option>
+                {/* Add more options here */}
+              </select>
 
-          <label htmlFor="firstName">What is Your First Name?</label>
-          <input type="text" id="firstName" placeholder="Your First Name Here..." />
+              <label htmlFor="firstName">What is Your First Name?</label>
+              <input type="text" id="firstName" placeholder="Your First Name Here..." />
 
-          <label htmlFor="lastName">What is Your Last Name?</label>
-          <input type="text" id="lastName" placeholder="Your Last Name Here..." />
+              <label htmlFor="lastName">What is Your Last Name?</label>
+              <input type="text" id="lastName" placeholder="Your Last Name Here..." />
 
-          <label htmlFor="birthdate">What is Your Birthdate?</label>
-          <input type="date" id="birthdate" />
+              <label htmlFor="birthdate">What is Your Birthdate?</label>
+              <input type="date" id="birthdate" placeholder="Your Birthdate Here..." />
 
-          <label htmlFor="email">What Email Should We Send Your Sketch To?</label>
-          <input type="email" id="email" placeholder="Your Email Address Here..." />
+              <label htmlFor="email">What Email Should We Send Your Sketch To?</label>
+              <input type="email" id="email" placeholder="Your Email Address Here..." />
 
-          <div className="terms-and-agree">
-            <input type="checkbox" id="agree" />
-            <label htmlFor="agree">
-              I agree to the terms of service & privacy policy and also agree to receive my sketch via the email address I provided and consent to receive other relevant content from Soulmate Sketch via our newsletter which I can opt-out at any time.
-            </label>
-          </div>
+              <label htmlFor="agree">
+                <input type="checkbox" id="agree" />
+                I agree to the terms of service & privacy policy and also agree to receive my sketch via the email address I provided and consent to receive other relevant content from Soulmate Sketch via our newsletter which I can opt-out at anytime.
+              </label>
 
-          <button type="submit" className="submit-button">Get Your Soulmate Drawing</button>
-        </div>}
+              <button type="submit" className="submit-button">Get Your Soulmate Drawing</button>
+            </>
           </div>
         </div>
       )}
