6833035cc9f9fe118791b7746765e14736bdcabe
aliniluca <115418094+aliniluca@users.noreply.github.com>
2023-05-04 21:21:02 +0200

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,83 +1,63 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import './styles.css';
 
 const Form = () => {
-  const [agree, setAgree] = useState(false);
-  const [email, setEmail] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [birthdate, setBirthdate] = useState('');
+  const [showForm, setShowForm] = useState(false);
 
-  const submitForm = (e) => {
-    e.preventDefault();
-    axios.post('/api/soulmate', { email, firstName, birthdate });
+  const handleButtonClick = () => {
+    setShowForm(!showForm);
   };
 
   return (
-    <div>
-      <div className="container">
-        <h1>Get Your Soulmate Sketch</h1>
-        <form onSubmit={submitForm}>
-          <label>
-            Email:
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          </label>
-          <label>
-            First Name:
-            <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-          </label>
-          <label>
-            Birthdate:
-            <input type="date" value={birthdate} onChange={(e) => setBirthdate(e.target.value)} />
-          </label>
-          <label>
-            I agree to the terms and conditions:
-            <input type="checkbox" checked={agree} onChange={() => setAgree(!agree)} />
-          </label>
-          <div className="video-container">
-            <iframe
-              src="https://player.vimeo.com/video/616281920?h=9b8d1b79a6"
-              width="640"
-              height="360"
-              frameborder="0"
-              allow="autoplay; fullscreen; picture-in-picture"
-              allowfullscreen
-            ></iframe>
+    <div className="form-container">
+      <button className="cta-button" onClick={handleButtonClick}>
+        Yes, I want my future soulmate sketch now!
+      </button>
+      {showForm && (
+        <div className="form">
+          <h2>Just answer a few simple questions so we can draw your sketch accurately...</h2>
+
+          <label htmlFor="interestedIn">Who are You Interested In?</label>
+          <select id="interestedIn">
+            <option>I Like Men</option>
+            <option>I Like Women</option>
+          </select>
+
+          <label htmlFor="relationshipStatus">What is Your Relationship Status?</label>
+          <select id="relationshipStatus">
+            <option>Select Your Status</option>
+            {/* Add other options here */}
+          </select>
+
+          <label htmlFor="zodiacSign">What is Your Zodiac Sign?</label>
+          <select id="zodiacSign">
+            <option>Select Your Sign</option>
+            {/* Add other options here */}
+          </select>
+
+          <label htmlFor="firstName">What is Your First Name?</label>
+          <input type="text" id="firstName" placeholder="Your First Name Here..." />
+
+          <label htmlFor="lastName">What is Your Last Name?</label>
+          <input type="text" id="lastName" placeholder="Your Last Name Here..." />
+
+          <label htmlFor="birthdate">What is Your Birthdate?</label>
+          <input type="date" id="birthdate" />
+
+          <label htmlFor="email">What Email Should We Send Your Sketch To?</label>
+          <input type="email" id="email" placeholder="Your Email Address Here..." />
+
+          <div className="terms-and-agree">
+            <input type="checkbox" id="agree" />
+            <label htmlFor="agree">
+              I agree to the terms of service & privacy policy and also agree to receive my sketch via the email address I provided and consent to receive other relevant content from Soulmate Sketch via our newsletter which I can opt-out at any time.
+            </label>
           </div>
-          <button type="submit">Yes, I want my future soulmate sketch now!</button>
-        </form>
-      </div>
-      <div className="faq-section">
-        <h2>FAQ</h2>
-        <h3>How this Soulmate Sketch & Reading Works</h3>
-        {/* ... (other FAQ questions and answers) ... */}
-        <h3>What is All Included with My Sketch?</h3>
-        <p>
-          In addition to the sketch of your soulmate, you will receive a complete description of characteristics and qualities of this person that will help you connect when you are at the right time with this person.
-        </p>
-        <h3>Will I Know My Soulmate?</h3>
-        <p>
-          Many people have found that their sketch resembles someone that is currently close in their life, their current significant other / partner, or someone they admire or have feelings for.
-        </p>
-        <h3>What Can I Expect?</h3>
-        <p>
-          You can expect high quality work including a real sketch of the visions that I receive in my trance of connection with the infinite energy of the universe with a 100% satisfaction guarantee.
-        </p>
-      </div>
-      <div className="disclaimer">
-        <p>
-          Disclaimer: By participating in reading my services you acknowledge that I
-          am not a licensed psychologist, lawyer, or health care professional and my
-          services do not replace the care of lawyers, psychologists, or other
-          healthcare professionals. Tarot and numerology are in no way to be
-          construed or substituted as psychological counseling or any other type of
-          therapy or medical advice. I will at all times exercise my best
-          professional efforts, skills, and care. However, I cannot guarantee the
-outcome of reading efforts and/or recommendations on my website and my comments about the outcome are expressions of opinion only. I cannot make any guarantees other than to deliver the services purchased as described. All my sketches and readings are intended for entertainment purposes only and may not be unique.
-</p>
-</div>
-</div>
-);
+
+          <button type="submit" className="submit-button">Get Your Soulmate Drawing</button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Form;
