09f54615aeaa699cdd6c8b95786322054776aad4
aliniluca <115418094+aliniluca@users.noreply.github.com>
2023-05-05 15:33:28 +0300

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,70 +1,94 @@
 import React, { useState } from 'react';
-import './styles.css';
+import './Form.css';
+import Modal from 'react-modal';
 
-const Form = () => {
-  const [showForm, setShowForm] = useState(false);
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
-  const handleButtonClick = () => {
-    setShowForm(!showForm);
+Modal.setAppElement('#root');
+
+function Form() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
   };
 
   const closeModal = () => {
-    setShowForm(false);
+    setIsOpen(false);
   };
 
+  const zodiacSigns = [
+    'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'
+  ];
+
   return (
-    <div className="form-container">
-      <button className="cta-button" onClick={handleButtonClick}>
-        Yes, I want my future soulmate sketch now!
-      </button>
-      {showForm && (
-        <div className="modal">
-          <div className="form">
-            <button className="close-modal" onClick={closeModal}>X</button>
-            <h2>Just answer a few simple questions so we can draw your sketch accurately...</h2>
-            <>
-              <label htmlFor="interestedIn">Who are You Interested In?</label>
-              <select id="interestedIn">
-                <option>I Like Men</option>
-                <option>I Like Women</option>
-              </select>
-
-              <label htmlFor="relationshipStatus">What is Your Relationship Status?</label>
-              <select id="relationshipStatus">
-                <option>Select Your Status</option>
-                {/* Add more options here */}
-              </select>
-
-              <label htmlFor="zodiacSign">What is Your Zodiac Sign?</label>
-              <select id="zodiacSign">
-                <option>Select Your Sign</option>
-                {/* Add more options here */}
-              </select>
-
-              <label htmlFor="firstName">What is Your First Name?</label>
-              <input type="text" id="firstName" placeholder="Your First Name Here..." />
-
-              <label htmlFor="lastName">What is Your Last Name?</label>
-              <input type="text" id="lastName" placeholder="Your Last Name Here..." />
-
-              <label htmlFor="birthdate">What is Your Birthdate?</label>
-              <input type="date" id="birthdate" placeholder="Your Birthdate Here..." />
-
-              <label htmlFor="email">What Email Should We Send Your Sketch To?</label>
-              <input type="email" id="email" placeholder="Your Email Address Here..." />
-
-              <label htmlFor="agree">
-                <input type="checkbox" id="agree" />
-                I agree to the terms of service & privacy policy and also agree to receive my sketch via the email address I provided and consent to receive other relevant content from Soulmate Sketch via our newsletter which I can opt-out at anytime.
-              </label>
-
-              <button type="submit" className="submit-button">Get Your Soulmate Drawing</button>
-            </>
-          </div>
-        </div>
-      )}
+    <div>
+      <button onClick={openModal}>Yes, I want my future soulmate sketch now!</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <button onClick={closeModal}>&times;</button>
+        <h2>Just answer a few simple questions so we can draw your sketch accurately...</h2>
+        <form>
+          <label htmlFor="interestedIn">Who are You Interested In?</label>
+          <select id="interestedIn">
+            <option>I Like Men</option>
+            <option>I Like Women</option>
+          </select>
+          <br />
+          <label htmlFor="relationshipStatus">What is Your Relationship Status?</label>
+          <select id="relationshipStatus">
+            <option>Select Your Status</option>
+            <option>Single</option>
+            <option>In a Relationship</option>
+            <option>Married</option>
+            <option>Separated</option>
+            <option>Divorced</option>
+            <option>Widowed</option>
+          </select>
+          <br />
+          <label htmlFor="zodiacSign">What is Your Zodiac Sign?</label>
+          <select id="zodiacSign">
+            <option>Select Your Sign</option>
+            {zodiacSigns.map((sign, index) => (
+              <option key={index} value={sign}>{sign}</option>
+            ))}
+          </select>
+          <br />
+          <label htmlFor="firstName">What is Your First Name?</label>
+          <input type="text" id="firstName" />
+          <br />
+          <label htmlFor="lastName">What is Your Last Name?</label>
+          <input type="text" id="lastName" />
+          <br />
+          <label htmlFor="birthdate">What is Your Birthdate?</label>
+          <input type="date" id="birthdate" />
+          <br />
+          <label htmlFor="email">What Email Should We Send Your Sketch To?</label>
+          <input type="email" id="email" />
+          <br />
+          <label>
+            <input type="checkbox" />
+            I agree to the terms of service & privacy policy and also agree to receive my sketch via the email address I provided and consent to receive other relevant content from Soulmate Sketch via our newsletter which I can opt-out at anytime.
+          </
+          </label>
+          <button type="submit">Get Your Soulmate Drawing</button>
+        </form>
+      </Modal>
     </div>
   );
-};
+}
 
 export default Form;
