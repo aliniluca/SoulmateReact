7a3f49180bc95fd898b6510cb4420bc614a893bc
aliniluca <115418094+aliniluca@users.noreply.github.com>
2023-05-05 16:18:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React from 'react';
+import './App.css';
 import Form from './Form';
-import GothicThemeProvider from './GothicThemeProvider';
-import { Global, css } from '@emotion/react';
+import divineDarlingLogo from './divine-darling-logo.png'; // Import the logo image file
 
-const globalStyles = css`
-  body {
-    margin: 0;
-    font-family: 'Creepster', cursive;
-    background-color: #000;
-    color: #fff;
-  }
-`;
-
-const App = () => {
+function App() {
   return (
-    <GothicThemeProvider>
-      <Global styles={globalStyles} />
-      <div>
-        <h1>Divine Darling's Twinflame Reading</h1>
-        <Form />
+    <div className="container">
+      <img
+        src={divineDarlingLogo}
+        alt="DivineDarling Logo"
+        style={{ display: 'block', width: '100%', maxWidth: '250px', margin: '0 auto' }}
+      />
+      <h1>Are You Ready to Finally Meet Your True Soulmate?</h1>
+      <div className="video-container">
+        <iframe
+          src="https://player.vimeo.com/video/616281920"
+          frameBorder="0"
+          allow="autoplay; fullscreen; picture-in-picture"
+          allowFullScreen
+          title="Vimeo Video"
+        ></iframe>
       </div>
-    </GothicThemeProvider>
+      <Form />
+
+      {/* As Seen On section */}
+      <div className="as-seen-on">
+        <h2>As Seen On</h2>
+        {/* Add logos of famous news sites and TV shows here */}
+        {/* Example: <img src={newsSiteLogo} alt="News Site" /> */}
+      </div>
+    </div>
   );
-};
+}
 
 export default App;
